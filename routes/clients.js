937d670c2da6e16d65aadfa14aa7980b8f59843d
JulesCharles <julees.charles@gmail.com>
2022-03-02 16:33:05 +0100

    merge de dev

--- routes/clients.js
+++ routes/clients.js
@@ -1,7 +1,7 @@
-const clientsRouter = require('express').Router();
+const clientsRouter = require('express').Router()
 
-clientsRouter.get("/", (req, res) => {
-    res.send("clients")
+clientsRouter.get('/', (req, res) => {
+  res.send('clients')
 })
 
 module.exports = clientsRouter
