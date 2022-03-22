af18765febcf4fdddede04538c8171abd52a84c6
MUNSCH <thomas-munsch_student2021@wilder.school>
2022-03-22 10:46:59 +0100

    finished GET route /form

--- routes/clients.js
+++ routes/clients.js
@@ -14,6 +14,112 @@ clientsRouter.get('/', (req, res) => {
   })
 })
 
+clientsRouter.get('/form', (req, res) => {
+  let id = req.params.id
+  let sqlprojects = 'SELECT id, numProject FROM projects;'
+  let sqlfun = 'SELECT id, fonctionName FROM fonction;'
+  let sqlfavc = 'SELECT id, contactTypeName FROM contactType;'
+  let sqlkob = 'SELECT id, companyTypeName FROM companyType;'
+  let sqldeserv = 'SELECT id, serviceName FROM service;'
+  let sqllang = 'SELECT id, languagesName FROM languages;'
+  let projects = []
+  let fonction = []
+  let contactType = []
+  let companyType = []
+  let service = []
+  let languages = []
+
+  connection.query(sqlprojects, (errprojects, resultprojects) => {
+    if (errprojects) {
+      console.error(errprojects)
+      res.status(500).send('Error requesting form datas')
+    } else {
+      resultprojects.forEach(project =>
+        projects.push({
+          id: project.id,
+          value: project.numProject,
+          label: project.numProject
+        })
+      )
+      connection.query(sqllang, (errlang, resultlang) => {
+        if (errlang) {
+          console.error(errlang)
+          res.status(500).send('Error requesting form datas')
+        } else {
+          resultlang.forEach(language =>
+            languages.push({
+              if: language.id,
+              value: language.languagesName,
+              label: language.languagesName
+            })
+          )
+        }
+      })
+      connection.query(sqlfun, (errfun, resultfun) => {
+        if (errfun) {
+          console.error(errfun)
+        } else {
+          resultfun.forEach(fun =>
+            fonction.push({
+              id: fun.id,
+              value: fun.fonctionName,
+              label: fun.fonctionName
+            })
+          )
+        }
+      })
+      connection.query(sqlfavc, (errfavc, resultfavc) => {
+        if (errfavc) {
+          console.error(errfavc)
+        } else {
+          resultfavc.forEach(favc =>
+            contactType.push({
+              id: favc.id,
+              value: favc.contactTypeName,
+              label: favc.contactTypeName
+            })
+          )
+          connection.query(sqlkob, (errkob, resultkob) => {
+            if (errkob) {
+              console.error(errkob)
+            } else {
+              resultkob.forEach(kob =>
+                companyType.push({
+                  id: kob.id,
+                  value: kob.companyTypeName,
+                  label: kob.companyTypeName
+                })
+              )
+              connection.query(sqldeserv, (errdeserv, resultdeserv) => {
+                if (errdeserv) {
+                  console.error(errdeserv)
+                } else {
+                  resultdeserv.forEach(deserv => {
+                    service.push({
+                      id: deserv.id,
+                      value: deserv.serviceName,
+                      label: deserv.serviceName
+                    })
+                  })
+                  const options = {
+                    projects: [...projects],
+                    fonction: [...fonction],
+                    contactType: [...contactType],
+                    companyType: [...companyType],
+                    service: [...service],
+                    languages: [...languages]
+                  }
+                  res.status(200).json(options)
+                }
+              })
+            }
+          })
+        }
+      })
+    }
+  })
+})
+
 clientsRouter.post('/', (req, res) => {
   const {
     firstname,
@@ -58,25 +164,34 @@ clientsRouter.post('/', (req, res) => {
       res.status(500).send('Error requesting POST clients')
     } else {
       const id = result.insertId
-      let datas2 = [id, contactType_id]
-      connection.query(sql2, datas2, (err2, result2) => {
+      let ctc = []
+      for (let i = 0; i < contactType_id.length; i++) {
+        ctc.push([id, contactType_id[i]])
+      }
+      connection.query(sql2, [ctc], (err2, result2) => {
         if (err2) {
           console.error(err2)
           res.status(500).send('Error requesting POST2 clients')
         } else {
-          let datas3 = [id, languages_id]
-          connection.query(sql3, datas3, (err3, result3) => {
+          let lan = []
+          for (let i = 0; i < languages_id.length; i++) {
+            lan.push([id, languages_id[i]])
+          }
+          connection.query(sql3, [lan], (err3, result3) => {
             if (err3) {
               console.error(err3)
               res.status(500).send('Error requesting POST3 clients')
             } else {
-              let datas4 = [id, service_id]
-              connection.query(sql4, datas4, (err4, result4) => {
+              let svc = []
+              for (let i = 0; i < service_id.length; i++) {
+                svc.push([id, service_id[i]])
+              }
+              connection.query(sql4, [svc], (err4, result4) => {
                 if (err4) {
                   console.error(err4)
                   res.status(500).send('Error requesting POST4 clients')
                 } else {
-                  res.status(200).json(result)
+                  res.status(200).json(result4)
                 }
               })
             }
