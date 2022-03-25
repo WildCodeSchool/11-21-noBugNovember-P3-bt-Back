58818d38bd6ba7157044e6023389cee7ba998b29
Anouchka <anouchka.maurer@protonmail.com>
2022-03-25 09:52:41 +0100

    put et delete projexperts

--- routes/projexperts.js
+++ routes/projexperts.js
@@ -4,8 +4,8 @@ const connection = require('../config/db.js')
 projexpertsRouter.get('/:id', (req, res) => {
   let sql =
     "SELECT group_concat(DISTINCT ehp.experts_id SEPARATOR ', ') AS experts_id FROM experts_has_projects AS ehp WHERE ehp.projects_id = ? GROUP BY ehp.projects_id;"
-  const projectId = req.params.id
-  connection.query(sql, [projectId], (err, result) => {
+  const expertId = req.params.id
+  connection.query(sql, [expertId], (err, result) => {
     let answer = []
     if (result.length) {
       let sql2 =
@@ -19,7 +19,7 @@ projexpertsRouter.get('/:id', (req, res) => {
       }
 
       for (let i = 0; i < resArray.length; i++) {
-        let datas = [resArray[i], projectId]
+        let datas = [resArray[i], expertId]
 
         connection.query(sql2, datas, (err, result) => {
           if (err) {
@@ -27,59 +27,8 @@ projexpertsRouter.get('/:id', (req, res) => {
             res.status(500).send('Error requesting expert')
           } else {
             answer.push(result[0])
-            console.log(answer)
             if (answer.length === results.length) {
               res.status(200).json(answer)
-            }
-          }
-        })
-      }
-    } else {
-      res.status(200).json(answer)
-    }
-  })
-})
-
-projexpertsRouter.get('/form/:id', (req, res) => {
-  let sql =
-    "SELECT group_concat(DISTINCT ehp.experts_id SEPARATOR ', ') AS experts_id FROM experts_has_projects AS ehp WHERE ehp.projects_id = ? GROUP BY ehp.projects_id;"
-  const projectId = req.params.id
-  connection.query(sql, [projectId], (err, result) => {
-    let array = []
-    if (result.length) {
-      let sql2 =
-        'SELECT e.id, ehp.answer, ehp.preferedItwDay, ehp.factuByExpert, FROM experts_has_projects AS ehp LEFT JOIN experts AS e ON ehp.experts_id = e.id LEFT JOIN experts_has_projects ON ehp.experts_id = e.id WHERE e.id = ? AND ehp.projects_id = ? GROUP BY e.id;'
-
-      let resArray = []
-      let results = result[0].experts_id.split(', ')
-
-      for (let i = 0; i < results.length; i++) {
-        resArray.push(results[i])
-      }
-
-      for (let i = 0; i < resArray.length; i++) {
-        let datas = [resArray[i], projectId]
-
-        connection.query(sql2, datas, (err, result) => {
-          if (err) {
-            console.error(err)
-            res.status(500).send('Error requesting expert')
-          } else {
-            array.push(result[0])
-            console.log(array)
-            if (array.length === results.length) {
-              const id = result[0].id
-              const answer = result[0].answer
-              const itwDay = result[0].preferedItwDay
-              const factu = result[0].factuByExpert
-
-              const res = {
-                id: id,
-                answer: answer,
-                itwDay: itwDay,
-                factu: factu
-              }
-              res.status(200).json(res)
             }
           }
         })
@@ -104,6 +53,101 @@ projexpertsRouter.post('/', (req, res) => {
       res.status(500).send('Error requesting POST expert has project')
     } else {
       res.status(200).json(result)
+    }
+  })
+})
+
+projexpertsRouter.put('/form/:id', (req, res) => {
+  const experts_id = parseInt(req.params.id)
+  const projects_id = parseInt(req.body.projects_id)
+  let sql =
+    'SELECT ehp.experts_id, ehp.answer, ehp.preferedItwDay, ehp.factuByExpert FROM experts_has_projects AS ehp WHERE ehp.experts_id = ? AND ehp.projects_id = ? GROUP BY ehp.experts_id;'
+  connection.query(sql, [experts_id, projects_id], (err, result) => {
+    if (err) {
+      console.error(err)
+      res.status(500).send('Error requesting expert')
+    } else {
+      const id = result[0].id
+      const itwDay = result[0].preferedItwDay
+      const factu = result[0].factuByExpert
+      let answer = undefined
+      if (result[0].answer) {
+        if (result[0].answer === 1) {
+          answer = { id: 1, value: 'Yes', label: 'Yes' }
+        } else {
+          answer = { id: 2, value: 'No', label: 'No' }
+        }
+      }
+      answer = result[0].answer
+
+      const datas = {
+        id: id,
+        answer: answer,
+        itwDay: itwDay,
+        factu: factu
+      }
+      res.status(200).json(datas)
+    }
+  })
+})
+
+projexpertsRouter.put('/:id', async (req, res) => {
+  const experts_id = parseInt(req.params.id)
+  const body = req.body
+  const projects_id = parseInt(body.idProject)
+  const sqlData = {
+    answer: body.answer,
+    factuByExpert: body.factu,
+    preferedItwDay: body.itwDay
+  }
+
+  let sql = 'UPDATE experts_has_projects SET '
+
+  const datas = Object.entries(sqlData)
+  let myArray = datas
+    .filter(element => element[1] !== undefined)
+    .filter(element => element[1] !== '')
+    .filter(element => element[1].length !== 0)
+    .filter(element => element[1].length !== null)
+
+  let sqlVal = []
+
+  myArray.map((array, i, arr) => {
+    if (i < arr.length - 1) {
+      sql += `${array[0]} = ?, `
+      sqlVal.push(array[1])
+    } else {
+      sql += `${array[0]} = ? `
+      sqlVal.push(array[1])
+    }
+  })
+
+  sql += 'WHERE experts_id = ? AND projects_id = ?;'
+  sqlVal.push(experts_id, projects_id)
+
+  if (myArray.length > 0) {
+    connection.query(sql, sqlVal, (err, result) => {
+      if (err) {
+        console.error(err)
+        res.status(500).send('Error updating sql projexperts')
+      } else {
+        res.send(result)
+      }
+    })
+  }
+})
+
+projexpertsRouter.put('/delete/:id', (req, res) => {
+  const experts_id = parseInt(req.params.id)
+  const projects_id = parseInt(req.body.projects_id)
+  const sql =
+    'DELETE FROM experts_has_projects WHERE experts_id = ? AND projects_id = ?;'
+  connection.query(sql, [experts_id, projects_id], (err, result) => {
+    if (err) {
+      console.error(err)
+      res.status(500).send('Error Delete projexpert')
+    } else {
+      res.sendStatus(200)
     }
   })
 })
